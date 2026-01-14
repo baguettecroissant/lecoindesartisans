bd84f46f0e704155a66621f2e45ff2b42f95ad37
baguettecroissant <wade.perso@gmail.com>
2026-01-14 10:44:33 +0400

    feat: Add Google Search Console verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,9 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
+  verification: {
+    google: "D287S-sbSARe7c0sKHHY0bw8ajoi5tW-ULAbt3Z4ZH4",
+  },
 };
 
 export default function RootLayout({
